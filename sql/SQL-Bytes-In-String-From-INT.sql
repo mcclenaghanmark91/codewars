925c50970ec5e3cfd882e92930c1039dffbf4dd0
mcclenaghanmark91 <58256834+mcclenaghanmark91@users.noreply.github.com>
2023-11-15 13:07:01 +0000

    Create SQL-Bytes-In-String-From-INT.sql
    NOTE: Codewars challenges are done using MYSQL

---
+++ sql/SQL-Bytes-In-String-From-INT.sql
@@ -0,0 +1,6 @@
+SELECT  LENGTH(number1::TEXT) AS octnum1,
+        LENGTH(number2::TEXT) AS octnum2,
+        LENGTH(number3::TEXT) AS octnum3,
+        LENGTH(number4::TEXT) AS octnum4,
+        LENGTH(number5::TEXT) AS octnum5
+FROM    numbers
